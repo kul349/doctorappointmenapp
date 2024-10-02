147f0454ad9823842e646b9a32fa4aa2b61a353e
Kul Karki <Kkarki349@rku.ac.in>
2024-10-02 20:40:45 +0530

    checksession

--- lib/services/token_service.dart
+++ lib/services/token_service.dart
@@ -1,5 +1,8 @@
 // token_service.dart
+import 'package:doctorappointmenapp/routes/app_routes.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get/get.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class TokenService {
   final _storage = FlutterSecureStorage();
@@ -31,6 +34,46 @@ class TokenService {
       print('Token removed successfully'); // Debugging line
     } catch (e) {
       print('Failed to remove token: $e');
+    }
+  }
+
+  // check the session of the token
+  Future<void> checkSessionAndRedirect() async {
+    String? token = await getToken();
+
+    if (token != null) {
+      bool isExpired = JwtDecoder.isExpired(token);
+      Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+
+      if (isExpired) {
+        // Check if the token belongs to a doctor or patient by verifying the keys in the token
+        final bool isDoctor = decodedToken.containsKey('doctorName') ||
+            decodedToken.containsKey('specialization');
+        final bool isPatient =
+            decodedToken.containsKey('userName') && !isDoctor;
+
+        if (isDoctor) {
+          // Session expired, redirect to doctor login
+          await removeToken(); // Clear the token
+          Get.offAllNamed(AppRoutes
+              .DOCTOR_LOGIN); // Assuming you're using GetX for navigation
+        } else if (isPatient) {
+          // Session expired, redirect to patient login
+          await removeToken(); // Clear the token
+          Get.offAllNamed(AppRoutes
+              .PATIENT_LOGIN); // Assuming you're using GetX for navigation
+        } else {
+          print(
+              'Unknown token type, unable to identify user as doctor or patient.');
+        }
+      } else {
+        print('Token is still valid.');
+        // Proceed with normal app flow as the token is valid
+      }
+    } else {
+      print('No token found, redirect to login.');
+      // Redirect to a common login page if no token is found
+      Get.offAllNamed(AppRoutes.HOME); // Or handle based on your app's flow
     }
   }
 }
