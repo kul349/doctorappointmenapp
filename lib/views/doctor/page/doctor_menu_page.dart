ba3e7b7934c496d3fc7a083eea0985c148939c00
Kul Karki <Kkarki349@rku.ac.in>
2024-10-23 14:02:47 +0530

    whole ui changed

--- lib/views/doctor/page/doctor_menu_page.dart
+++ lib/views/doctor/page/doctor_menu_page.dart
@@ -1,6 +1,6 @@
 import 'package:doctorappointmenapp/controllers/patient_getalldoctor_controller.dart';
 import 'package:doctorappointmenapp/models/doctor/gride_model.dart';
-import 'package:doctorappointmenapp/themes/app_theme.dart';
+import 'package:doctorappointmenapp/views/patient/doctor_fav_page.dart';
 import 'package:doctorappointmenapp/views/patient/doctor_profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -23,6 +23,7 @@ class DoctorListPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(specialization.toUpperCase()),
         centerTitle: true,
+        backgroundColor: Colors.green, // Set dominant color as green
       ),
       body: Obx(() {
         // Show a loading indicator while data is being fetched
@@ -44,92 +45,143 @@ class DoctorListPage extends StatelessWidget {
             final DoctorModel doctor = doctorController.filteredDoctors[index];
 
             return Card(
-              color: mpurple,
-              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 02),
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: ListTile(
-                contentPadding: const EdgeInsets.only(left: 4.0, right: 4.0),
-                leading: CircleAvatar(
-                  radius: 45,
-                  backgroundImage: doctor.avatar.isNotEmpty
-                      ? NetworkImage(doctor.avatar)
-                      : null,
-                  child: doctor.avatar.isEmpty
-                      ? const Icon(Icons.person, size: 30)
-                      : null,
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              elevation: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: Text(
-                        doctor.fullName,
-                        style: const TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.bold),
-                        overflow: TextOverflow
-                            .ellipsis, // Handle long names gracefully
+                    // Doctor's image on the left side
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey[200], // Placeholder background color
+                        image: DecorationImage(
+                          image: doctor.avatar.isNotEmpty
+                              ? NetworkImage(doctor.avatar)
+                              : const AssetImage('assets/placeholder.png')
+                                  as ImageProvider,
+                          fit:
+                              BoxFit.cover, // Ensures the image scales properly
+                        ),
                       ),
                     ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 5,
-                          vertical: 5), // Adjust padding as needed
-                      decoration: BoxDecoration(
-                        color: kWhiteColor,
-                        borderRadius:
-                            BorderRadius.circular(17), // Rounded corners
-                      ),
-                      child: Row(
+                    const SizedBox(width: 10),
+
+                    // Doctor's details on the right side
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            doctor.averageRating.toStringAsFixed(1),
-                            style: const TextStyle(
-                              fontSize: 20, // Font size for rating
-                              color: Colors.yellow, // Text color
-                            ),
+                          // Doctor's name with favorite icon
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  doctor.fullName,
+                                  style: const TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.red,
+                                ),
+                                onPressed: () {
+                                  // Logic to mark as favorite
+                                  Get.to(() => FavoritesPage());
+                                },
+                              ),
+                            ],
                           ),
-                          const SizedBox(width: 8),
-                          const Icon(
-                            Icons.star,
-                            color: Colors.black, // Icon color
-                            size: 28,
+                          const SizedBox(height: 10),
+
+                          // Specialization
+                          Text(
+                            doctor.specialization,
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                          const SizedBox(height: 5),
+
+                          // Clinic Address
+                          Text(
+                            'Clinic Address: ${doctor.clinicAddress}',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 5),
+
+                          // Availability status
+                          Text(
+                            'Availability: ${doctor.availabilityStatus}',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 10),
+
+                          // Rating, Star, and Book Button
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // Display rating and stars
+                              Row(
+                                children: [
+                                  Text(
+                                    doctor.averageRating.toStringAsFixed(1),
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.yellow,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                    size: 28,
+                                  ),
+                                ],
+                              ),
+
+                              // Book button
+                              ElevatedButton(
+                                onPressed: () {
+                                  // Navigate to the DoctorProfileDetails
+                                  print(
+                                      'Navigating to DoctorProfileDetails with doctor: ${doctor.id}');
+                                  Get.to(
+                                    () => DoctorProfileDetails(),
+                                    arguments:
+                                        doctor, // Pass the DoctorModel object
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      Colors.green, // Set button color to green
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Book',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ),
                   ],
                 ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 3),
-                    Text(
-                      doctor.specialization,
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                    const SizedBox(height: 5),
-
-                    Text(
-                      'Availability: ${doctor.availabilityStatus}',
-                      style: const TextStyle(fontSize: 20),
-                    ), // Display availability status
-                    Text(
-                      'clinicAddress: ${doctor.clinicAddress}',
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  // Navigate to the DoctorDetailPage with the selected doctor
-                  print(
-                      'Navigating to DoctorProfileDetails with doctor: ${doctor.id}');
-                  Get.to(
-                    () => DoctorProfileDetails(),
-                    arguments: doctor, // Pass the DoctorModel object
-                  );
-                },
               ),
             );
           },
